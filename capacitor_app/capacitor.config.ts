f4df5e0e5446884a7af943eac5b287209c57b84a
Utkarsh Shukla <shuklajimasthai@gmail.com>
2026-06-30 11:42:04 +0530

    Configure Android app to automatically sync with live website

--- capacitor_app/capacitor.config.ts
+++ capacitor_app/capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sjs.erp',
   appName: 'SJS-School-ERP',
-  webDir: 'web'
+  webDir: 'web',
+  server: {
+    url: 'https://sjs-school.vercel.app'
+  }
 };
 
 export default config;
